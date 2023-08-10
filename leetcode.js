d1f6447388c3d16263a4a39cba8f7c279879465d
alessio.leodori <alessio.leodori@gmail.com>
2023-08-10 07:52:48 -0400

    completed Merge Sorted Array

--- leetcode.js
+++ leetcode.js
@@ -196,3 +196,19 @@ nums2.length == n
 -109 <= nums1[i], nums2[j] <= 109
  
 */
+
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m);
+  nums2.splice(n);
+  nums1.push(...nums2);
+  return nums1.sort((a, b) => a - b);
+};
+
+const nums1 = [1, 2, 3, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 5, 6];
+const n = 3;
+
+console.log(merge(nums1, m, nums2, n));
+
+// Submission accepted ✅
