6ff74a4cc023624fc4576af0d40fc90c0ba6034e
alessio.leodori <alessio.leodori@gmail.com>
2023-08-19 18:56:41 +0200

    added problem text for Majority Element

--- leetcode.js
+++ leetcode.js
@@ -518,3 +518,34 @@ var missingNumber = function (nums) {
 nums = [3, 0, 1];
 
 console.log(missingNumber(nums));
+
+// Submission accepted ✅
+
+/*
+
+169. Majority Element 💡
+
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+
+*/
