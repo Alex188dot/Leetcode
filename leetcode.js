cf30177f1cc187c1c969df2517447dc12933ba8d
alessio.leodori <alessio.leodori@gmail.com>
2023-08-21 14:08:14 +0200

    submission accepted for Longest common prefix

--- leetcode.js
+++ leetcode.js
@@ -599,3 +599,20 @@ Constraints:
 strs[i] consists of only lowercase English letters.
 
 */
+
+var longestCommonPrefix = function (strs) {
+  let prefix = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (i === strs[j].length || strs[0][i] !== strs[j][i]) {
+        return prefix;
+      }
+    }
+    prefix += strs[0][i];
+  }
+  return prefix;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+
+// Submission accepted ✅
